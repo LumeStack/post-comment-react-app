d1599dbe834c55664805ab8c1895c7b64f5dca32
AguiarJuliana <julianaaguiarquinto@gmail.com>
2025-04-11 10:06:41 -0300

    First scenario cypress automation

--- cypress/mainspec.cy.js
+++ cypress/mainspec.cy.js
@@ -1,6 +1,6 @@
 describe('mainspec.cy.js', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:5173/')
+    cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
     cy.get('data-qa="Comment-input"]').first().type('Test 123')
     cy.get('[data-qa="publish-button"]').first().click()
