a362ddfa17b9c201a111e722f17384007e76a03b
Gabriel <gabrielhl25@hotmail.com>
2025-06-25 14:18:11 -0300

    Change comment text

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -1,12 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Post Comment App', () => {
+  it('Add and delete a comment', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="comment-input"]').first().type('Nice post!')
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
+    cy.get('[data-qa="comment-text"]').contains('Nice post!')
     cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
+      if ($element.text() === 'Nice post!') {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     })
