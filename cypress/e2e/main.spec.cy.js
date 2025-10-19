ce5d1501cbafc3de673695d0c21adfb45ef08da6
rsantana-tech <rogeriosantana000@gmail.com>
2025-10-19 19:46:08 -0300

    test: keep comment text consistent in e2e

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -1,12 +1,14 @@
 describe('template spec', () => {
   it('passes', () => {
+    const commentText = 'Test 12345'
+
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="comment-input"]').first().type(commentText)
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
+    cy.get('[data-qa="comment-text"]').contains(commentText)
     cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
+      if ($element.text() === commentText) {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     })
