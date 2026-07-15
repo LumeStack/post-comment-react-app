7ab4f4a094bcb50387e8ac2a676858b370f6043c
RaphaelCoelho <raphaelmnc@gmail.com>
2026-07-15 17:42:49 -0300

    alterei comentario

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,11 +2,11 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('bla bla bla')
+    cy.get('[data-qa="comment-input"]').first().type('test')
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('bla bla bla')
+    cy.get('[data-qa="comment-text"]').contains('test')
     cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'bla bla bla') {
+      if ($element.text() === 'test') {
       cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     })
