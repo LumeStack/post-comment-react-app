db8ff9df620dc91f53e3dd76ae38f06e8e851481
RaphaelCoelho <raphaelmnc@gmail.com>
2026-07-15 17:16:24 -0300

    First scenario cypressautomation

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,12 +2,12 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="comment-input"]').first().type('bla bla bla')
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
+    cy.get('[data-qa="comment-text"]').contains('bla bla bla')
     cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
-        cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
+      if ($element.text() === 'bla bla bla') {
+      cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     })
   })
